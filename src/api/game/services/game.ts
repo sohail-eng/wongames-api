e7ca7d936b4ed862a202ed01dcae172b03c64e7f
Pedro Estevão <contato@pedroestevao.com>
2023-09-28 22:22:25 -0300

    Update game.ts

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -180,10 +180,7 @@ async function createGames(products) {
                 await Promise.all(
                     product.screenshots.slice(0, 5).map((url) => 
                         setImage({
-                            image: `${url.replace(
-                                "{formatter}",
-                                "product_card_v2_mobile_slider_639"
-                            )}`,
+                            image: `${url.replace("{formatter}", "product_card_v2_mobile_slider_639").replace("\/", "/")}`,
                             game,
                             field: "gallery",
                         })
