ce3fb087199f7baabd11a372305f87b17f0660ac
Pedro-Estevao <contato@pedroestevao.com>
2024-07-30 15:06:39 -0300

    Upgrade Strapi

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,54 +1,15 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface PageButton extends Schema.Component {
-  collectionName: 'components_page_buttons';
+export interface PageSection extends Schema.Component {
+  collectionName: 'components_page_sections';
   info: {
-    displayName: 'Button';
-    icon: 'link';
+    displayName: 'Section';
+    icon: 'apps';
     description: '';
   };
   attributes: {
-    label: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Buy now'>;
-    link: Attribute.String & Attribute.Required;
-  };
-}
-
-export interface PageHighlight extends Schema.Component {
-  collectionName: 'components_page_highlights';
-  info: {
-    displayName: 'Highlight';
-    icon: 'star';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    subTitle: Attribute.Text & Attribute.Required;
-    background: Attribute.Media & Attribute.Required;
-    floatImage: Attribute.Media;
-    buttonLabel: Attribute.String & Attribute.Required;
-    buttonLink: Attribute.String & Attribute.Required;
-    alignment: Attribute.Enumeration<['right', 'left']> &
-      Attribute.DefaultTo<'right'>;
-  };
-}
-
-export interface PagePopularGames extends Schema.Component {
-  collectionName: 'components_page_popular_games';
-  info: {
-    displayName: 'PopularGames';
-    icon: 'crown';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
+    title: Attribute.String;
     highlight: Attribute.Component<'page.highlight'>;
-    games: Attribute.Relation<
-      'page.popular-games',
-      'oneToMany',
-      'api::game.game'
-    >;
   };
 }
 
@@ -71,27 +32,66 @@ export interface PageRibbon extends Schema.Component {
   };
 }
 
-export interface PageSection extends Schema.Component {
-  collectionName: 'components_page_sections';
+export interface PagePopularGames extends Schema.Component {
+  collectionName: 'components_page_popular_games';
   info: {
-    displayName: 'Section';
-    icon: 'apps';
+    displayName: 'PopularGames';
+    icon: 'crown';
     description: '';
   };
   attributes: {
-    title: Attribute.String;
+    title: Attribute.String & Attribute.Required;
     highlight: Attribute.Component<'page.highlight'>;
+    games: Attribute.Relation<
+      'page.popular-games',
+      'oneToMany',
+      'api::game.game'
+    >;
+  };
+}
+
+export interface PageHighlight extends Schema.Component {
+  collectionName: 'components_page_highlights';
+  info: {
+    displayName: 'Highlight';
+    icon: 'star';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    subTitle: Attribute.Text & Attribute.Required;
+    background: Attribute.Media<'images'> & Attribute.Required;
+    floatImage: Attribute.Media<'images'>;
+    buttonLabel: Attribute.String & Attribute.Required;
+    buttonLink: Attribute.String & Attribute.Required;
+    alignment: Attribute.Enumeration<['right', 'left']> &
+      Attribute.DefaultTo<'right'>;
+  };
+}
+
+export interface PageButton extends Schema.Component {
+  collectionName: 'components_page_buttons';
+  info: {
+    displayName: 'Button';
+    icon: 'link';
+    description: '';
+  };
+  attributes: {
+    label: Attribute.String &
+      Attribute.Required &
+      Attribute.DefaultTo<'Buy now'>;
+    link: Attribute.String & Attribute.Required;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'page.button': PageButton;
-      'page.highlight': PageHighlight;
-      'page.popular-games': PagePopularGames;
-      'page.ribbon': PageRibbon;
       'page.section': PageSection;
+      'page.ribbon': PageRibbon;
+      'page.popular-games': PagePopularGames;
+      'page.highlight': PageHighlight;
+      'page.button': PageButton;
     }
   }
 }
